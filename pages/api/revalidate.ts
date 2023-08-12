cbed56b44e9bffd9c76c828f534ff67aac24990d
Abdallah Antony <abdallahantony55.aa@gmail.com>
2023-08-12 09:56:42 -0400

    add static site

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -7,6 +7,6 @@ export default async function handler(req:any, res:any) {
       } catch (err) {
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
-        return res.status(500).send('Error revalidating');
+        return res.status(500).setHeader('Access-Control-Allow-Origin', '*').send('Error revalidating');
       }
 }

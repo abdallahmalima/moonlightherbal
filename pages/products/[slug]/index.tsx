e45fdecd4092fa46b0bb9a9ebc9ff2c5efe84154
Abdallah Antony <abdallahantony55.aa@gmail.com>
2023-08-13 02:40:41 -0400

    dddd

--- pages/products/[slug]/index.tsx
+++ pages/products/[slug]/index.tsx
@@ -67,7 +67,7 @@ export const getStaticPaths = async () => {
   }))
  
   // { fallback: false } means other routes should 404
-  return { paths, fallback: false }
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps(context) {

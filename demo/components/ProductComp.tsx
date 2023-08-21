cfa9ea61a4ef3ad86761221b3374b54ce549a48e
Abdallah Antony <abdallahantony55.aa@gmail.com>
2023-08-21 15:51:56 -0400

    effds

--- demo/components/ProductComp.tsx
+++ demo/components/ProductComp.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import ProductItem from './ProductItem';
 import Link from 'next/link';
+import { InputText } from 'primereact/inputtext';
+
 
 type ProductType={
   id:string;
@@ -11,11 +15,44 @@ type ProductType={
 }
 
 function ProductComp({isHome=false,products}:{isHome:boolean,products:ProductType[]}) {
-  
+  const [value,setValue]=useState<string | undefined>(undefined)
+  const [filteredProducts, setFilteredProducts] = useState<ProductType[]>(products); // Use filteredProducts state
+
+  // Handle search
+  const handleSearch = (searchValue: string) => {
+    const lowerCaseSearch = searchValue.toLowerCase();
+    const filtered = products.filter((product) => {
+      const nameMatches = product.name.toLowerCase().includes(lowerCaseSearch);
+      const diseaseMatches = product.diseases.some(
+        (disease) => disease.disease.toLowerCase().includes(lowerCaseSearch)
+      );
+      return nameMatches || diseaseMatches;
+    });
+    setFilteredProducts(filtered);
+  };
+
+
     return (
         <>
         <div className="container-xxl py-5">
+        <div className="flex justify-content-end p-3">
+        <span className="p-input-icon-left mr-8">
+         <i className="pi pi-search" />
+         <InputText 
+             style={{width:'23rem'}}
+             className="p-inputtext-md " 
+             placeholder="Tafuta Hapa mf: kifua,miguu,mafua" 
+             value={value}
+             onChange={(e) => {
+               setValue(e.target.value);
+               handleSearch(e.target.value);
+             }}
+             />
+       </span>
+       </div>
+
           <div className="container">
+            
             <div
               className="section-title text-center mx-auto wow fadeInUp"
               data-wow-delay="0.1s"
@@ -26,7 +63,7 @@ function ProductComp({isHome=false,products}:{isHome:boolean,products:ProductTyp
             </div>
             <div className="row g-4">
               
-              {products.map((product:ProductType)=>(
+            {filteredProducts.map((product: ProductType) => (
              <ProductItem key={product.id}
              id={product.id}
              name={product.name}
